c021d0a767575b9e9a3ba13331d7058fb15ac719
Karanko Andrei <andr.karanko@gmail.com>
2020-06-27 20:46:15 +0300

    Сделана кнопка возврата, плавный скроллинг и анимация сердца

--- js/main.js
+++ js/main.js
@@ -110,4 +110,21 @@ toggleSlide('.catalog-item__list-link');
     return false;
   });
 
+  //Кнопка up
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 1600) {
+      $('.pageup').fadeIn();
+    } else {
+      $('.pageup').fadeOut();
+    }
+  });
+
+  // Плавный скроллинг
+    $("a[href^='#']").click(function() {
+            let _href = $(this).attr("href");
+            $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+            return false;
+    });
+
 });
